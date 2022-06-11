de6123cdb094f28167f663a53626b2df09f19a38
Aashu Sailani <89945946+aashu015@users.noreply.github.com>
2022-06-12 04:07:47 +0530

    Updates

---
+++ precache-manifest.7ac2edb3948783279881d10df6513c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a20a8600c024ddfed4c2",
+    "url": "/aashu015/Reactjs.git/static/css/main.970624b9.chunk.css"
+  },
+  {
+    "revision": "a20a8600c024ddfed4c2",
+    "url": "/aashu015/Reactjs.git/static/js/main.a20a8600.chunk.js"
+  },
+  {
+    "revision": "17d417ca381548a94595",
+    "url": "/aashu015/Reactjs.git/static/js/1.17d417ca.chunk.js"
+  },
+  {
+    "revision": "f788d223e1030037507d",
+    "url": "/aashu015/Reactjs.git/static/js/runtime~main.f788d223.js"
+  },
+  {
+    "revision": "c79b35bda644ad04fac3e626b337d04c",
+    "url": "/aashu015/Reactjs.git/index.html"
+  }
+];
